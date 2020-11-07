c2e5257779988f49d897edc94cfe0dea2a040893
Shumax <maxcastro.mv@gmail.com>
2020-11-06 21:14:45 -0300

    Styles + Images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import '../styles/pages/Home.scss';
+import croqui from '../images/croqui-2.jpg';
+import croquiTree from '../images/croqui-3.jpg';
+import croquiFour from '../images/croqui-4.jpg';
 
 import Topbar from '../components/Topbar';
 import Footer from '../components/Footer';
@@ -10,14 +13,28 @@ function Home() {
     <>
       <Topbar />
       <div className="home">
-        <div className="container">
+        <nav className="container">
           <ul className="home__list">
             <li className="home__list--item">T-Shirts</li>
             <li className="home__list--item">Pants</li>
             <li className="home__list--item">Shoes</li>
             <li className="home__list--item">Bags</li>
           </ul>
-        </div>
+        </nav>
+
+        <ul className="home_slider">
+          <li>
+            <img alt="" src={croqui} />
+          </li>
+          <li>
+            <img alt="" src={croquiTree} />
+          </li>
+          <li>
+            <img alt="" src={croquiFour} />
+          </li>
+
+        </ul>
+
         <Footer />
       </div>
     </>
